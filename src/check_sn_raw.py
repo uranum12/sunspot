f6a6627f51ddfe4c0a7a277eaec69e0046274a2c
uranum12 <113694643+uranum12@users.noreply.github.com>
2023-09-21 12:04:38 +0000

    refactor check_sn_raw

--- src/check_sn_raw.py
+++ src/check_sn_raw.py
@@ -1,38 +1,68 @@
 import calendar
 import sys
+from functools import reduce
 from pathlib import Path
 
 import polars as pl
 
 import sn_type
 
+patterns: dict[sn_type.TimeType, dict[str, str]] = {
+    sn_type.TimeType.JST: {
+        "date": r"^\d{1,2}$",
+        "time": r"^\d{1,2}:\d{2}$",
+        "ng": r"^\d{1,2}$",
+        "nf": r"^\d{1,3}$",
+        "sg": r"^\d{1,2}$",
+        "sf": r"^\d{1,3}$",
+    },
+    sn_type.TimeType.UT: {
+        "date": r"^\d{1,2}$",
+        "time": r"^(-\d{1,2}|m?\d{1,2}:\d{2})$",
+        "ng": r"^\d{1,2}$",
+        "nf": r"^\d{1,3}$",
+        "sg": r"^\d{1,2}$",
+        "sf": r"^\d{1,3}$",
+    },
+}
 
-def contains_invalid_chars(col: str, pattern: str) -> pl.Expr:
-    return pl.col(col).str.count_matches(pattern) != 1
 
-
-def check_raw_jst(df: pl.DataFrame, days: int) -> pl.DataFrame:
+def check_raw(df: pl.DataFrame, pats: dict[str, str]) -> pl.DataFrame:
     return df.filter(
-        ~pl.col("date").cast(pl.UInt8).is_between(1, days)
-        | contains_invalid_chars("date", r"^\d{1,2}$")
-        | contains_invalid_chars("time", r"^\d{1,2}:\d{2}$")
-        | contains_invalid_chars("ng", r"^\d{1,2}$")
-        | contains_invalid_chars("nf", r"^\d{1,3}$")
-        | contains_invalid_chars("sg", r"^\d{1,2}$")
-        | contains_invalid_chars("sf", r"^\d{1,3}$"),
+        reduce(
+            lambda acc, cur: acc | cur,
+            [
+                pl.col(col).str.count_matches(pat) != 1
+                for col, pat in pats.items()
+            ],
+        ),
     )
 
 
-def check_raw_ut(df: pl.DataFrame, days: int) -> pl.DataFrame:
-    return df.filter(
-        ~pl.col("date").cast(pl.UInt8).is_between(1, days)
-        | contains_invalid_chars("date", r"^\d{1,2}$")
-        | contains_invalid_chars("time", r"^(-\d{1,2}|m?\d{1,2}:\d{2})$")
-        | contains_invalid_chars("ng", r"^\d{1,2}$")
-        | contains_invalid_chars("nf", r"^\d{1,3}$")
-        | contains_invalid_chars("sg", r"^\d{1,2}$")
-        | contains_invalid_chars("sf", r"^\d{1,3}$"),
-    )
+def check_date(df: pl.DataFrame, days: int) -> pl.DataFrame:
+    return df.filter(~pl.col("date").cast(pl.UInt8).is_between(1, days))
+
+
+def check_file(path: Path, days: int, pats: dict[str, str]) -> None:
+    file = pl.read_csv(path, infer_schema_length=0)
+
+    if (c := file.columns) != [
+        "date",
+        "time",
+        "ng",
+        "nf",
+        "sg",
+        "sf",
+        "remarks",
+    ]:
+        msg = f"invalid columns: {c}"
+        raise ValueError(msg)
+
+    if (date_checked := check_date(file, days)).height != 0:
+        raise ValueError(date_checked)
+
+    if (raw_checked := check_raw(file, pats)).height != 0:
+        raise ValueError(raw_checked)
 
 
 def main() -> int:
@@ -40,44 +70,17 @@ def main() -> int:
 
     for path in sn_path.glob("*-*.csv"):
         year, month = map(int, path.stem.split("-"))
-        try:
-            file = pl.read_csv(path, infer_schema_length=0)
-        except pl.ComputeError as e:
-            print(f"{year}/{month}")
-            print(e)
-            return 1
-
-        if (columns := file.columns) != [
-            "date",
-            "time",
-            "ng",
-            "nf",
-            "sg",
-            "sf",
-            "remarks",
-        ]:
-            print(f"{year}/{month}")
-            print(f"invalid columns: {columns}")
-            return 1
-
-        match sn_type.detect_time_type(year, month):
-            case sn_type.TimeType.JST:
-                raw_checked = check_raw_jst(
-                    file,
-                    calendar.monthrange(year, month)[1],
-                )
-            case sn_type.TimeType.UT:
-                raw_checked = check_raw_ut(
-                    file,
-                    calendar.monthrange(year, month)[1],
-                )
-            case _:
-                print(f"Err: not supported date for {year}/{month}")
-                continue
-        if raw_checked.height != 0:
-            print(f"{year}/{month}")
-            print(raw_checked)
-            return 1
+        if time_type := sn_type.detect_time_type(year, month):
+            try:
+                days = calendar.monthrange(year, month)[1]
+                check_file(path, days, patterns[time_type])
+            except (pl.ComputeError, ValueError) as e:
+                print(f"{year}/{month}")
+                print(e)
+                return 1
+        else:
+            print(f"Err: not supported date for {year}/{month}")
+            continue
 
     return 0
 

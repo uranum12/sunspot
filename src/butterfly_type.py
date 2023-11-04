188603ace4874a9ce14d44389f7e7a5e4fe8902d
uranum12 <113694643+uranum12@users.noreply.github.com>
2023-11-04 12:38:37 +0000

    add DateDelta kw_only option

--- src/butterfly_type.py
+++ src/butterfly_type.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 
-@dataclass(frozen=True, slots=True)
+@dataclass(frozen=True, slots=True, kw_only=True)
 class DateDelta:
     years: int | None = None
     months: int | None = None

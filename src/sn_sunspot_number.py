0cedf0f941c116bac91ac990312d2206220c294d
uranum12 <113694643+uranum12@users.noreply.github.com>
2023-11-16 13:15:29 +0000

    add compare fujimori and silso

--- src/sn_sunspot_number.py
+++ src/sn_sunspot_number.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import polars as pl
+from scipy import optimize
+from sklearn import metrics
 
 
-def main() -> None:
+def main() -> None:  # noqa: PLR0915
     path_fujimori = Path("out/sn/all.parquet")
     path_silso = Path("data/SN_m_tot_V2.0.txt")
     output_path = Path("out/sn")
@@ -48,14 +51,18 @@ def main() -> None:
     )
     print(df_silso)
 
-    df = df_fujimori.select("date", "tr").join(df_silso, on="date")
-    print(df)
+    df_joined = (
+        df_fujimori.select("date", "tr")
+        .rename({"tr": "fujimori"})
+        .join(df_silso, on="date")
+    )
+    print(df_joined)
 
     fig = plt.figure(figsize=(8, 5))
     ax = fig.add_subplot(111)
 
-    ax.plot(df["date"], df["tr"], lw=1, label="fujimori")
-    ax.plot(df["date"], df["silso"], lw=1, label="SILSO")
+    ax.plot(df_joined["date"], df_joined["fujimori"], lw=1, label="fujimori")
+    ax.plot(df_joined["date"], df_joined["silso"], lw=1, label="SILSO")
 
     ax.set_title("fujimori's sunspot number", y=1.1)
     ax.set_xlabel("year")
@@ -115,19 +122,91 @@ def main() -> None:
             pad_inches=0.1,
         )
 
+    popt, _ = optimize.curve_fit(
+        lambda x, a: x * a,
+        df_joined["silso"],
+        df_joined["fujimori"],
+    )
+    print(f"{popt[0]=}")
+    r2 = float(
+        metrics.r2_score(
+            df_joined["fujimori"],
+            df_joined["silso"] * popt[0],
+        ),
+    )
+    print(f"{r2=}")
+
     fig3 = plt.figure(figsize=(8, 5))
     ax = fig3.add_subplot(111)
 
+    ax.plot(
+        [0, 360],
+        np.poly1d([r2, 0])([0, 360]),
+        lw=1,
+        c="black",
+        zorder=1,
+    )
     ax.scatter(
-        df["silso"],
-        df["tr"],
+        df_joined["silso"],
+        df_joined["fujimori"],
         s=3,
         edgecolors="none",
+        zorder=2,
     )
+    ax.text(300, 160, f"$y={popt[0]:.5f}x$")
+    ax.text(300, 140, f"$R^2={r2:.5f}$")
 
-    ax.set_title("silso and fujimori")
-    ax.set_xlabel("silso")
+    ax.set_title("SILSO and fujimori")
+    ax.set_xlabel("SILSO")
     ax.set_ylabel("fujimori")
+
+    for ext in "pdf", "png":
+        fig3.savefig(
+            output_path / f"scatter.{ext}",
+            dpi=300,
+            bbox_inches="tight",
+            pad_inches=0.1,
+        )
+
+    df_ratio_diff = (
+        df_joined.lazy()
+        .with_columns(
+            (pl.col("fujimori") / pl.col("silso")).alias("ratio"),
+            (pl.col("fujimori") * r2 - pl.col("silso")).alias("diff"),
+        )
+        .sort("date")
+        .collect()
+    )
+    print(df_ratio_diff)
+
+    fig4 = plt.figure(figsize=(8, 8))
+    ax1 = fig4.add_subplot(211)
+    ax2 = fig4.add_subplot(212)
+
+    ax1.axhline(y=r2, c="black", lw=1, ls="--", zorder=1)
+    ax1.plot(df_ratio_diff["date"], df_ratio_diff["ratio"], lw=1, zorder=2)
+
+    ax1.set_title("ratio: fujimori / SILSO")
+    ax1.set_xlabel("year")
+    ax1.set_ylabel("ratio")
+    ax1.grid()
+
+    ax2.plot(df_ratio_diff["date"], df_ratio_diff["diff"], lw=1)
+
+    ax2.set_title("difference: fujimori * $R^2$ - SILSO")
+    ax2.set_xlabel("year")
+    ax2.set_ylabel("difference")
+    ax2.grid()
+
+    fig4.tight_layout()
+
+    for ext in "pdf", "png":
+        fig4.savefig(
+            output_path / f"ratio_diff.{ext}",
+            dpi=300,
+            bbox_inches="tight",
+            pad_inches=0.1,
+        )
 
     plt.show()
 

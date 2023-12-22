57deb82dd5af55deb24b0b67ea2b4fec22cb8192
uranum12 <113694643+uranum12@users.noreply.github.com>
2023-12-22 13:25:00 +0000

    add tox test

--- src/util/view_parquet.py
+++ src/util/view_parquet.py
@@ -3,11 +3,17 @@ from pathlib import Path
 
 import polars as pl
 
-if len(sys.argv) > 1:
-    path = Path(sys.argv[1])
-    print(
-        pl.scan_parquet(path)
-        .filter(~pl.all_horizontal(pl.all().is_null()))
-        .collect()
-        .glimpse(return_as_string=True),
-    )
+
+def main(argv: list[str]) -> None:
+    if len(argv) > 1:
+        path = Path(argv[1])
+        print(
+            pl.scan_parquet(path)
+            .filter(~pl.all_horizontal(pl.all().is_null()))
+            .collect()
+            .glimpse(return_as_string=True),
+        )
+
+
+if __name__ == "__main__":
+    main(sys.argv)

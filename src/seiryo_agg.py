12a4a8a5e7848d3706a9cbdc4e3dd16afb410ed3
uranum12 <113694643+uranum12@users.noreply.github.com>
2023-12-24 06:52:49 +0000

    fix seiryo_agg convert_lat function

--- src/seiryo_agg.py
+++ src/seiryo_agg.py
@@ -23,69 +23,68 @@ def convert_date(df: pl.LazyFrame) -> pl.LazyFrame:
     )
 
 
-def convert_lat(df: pl.LazyFrame) -> pl.LazyFrame:
-    for coord in ["lat", "lon"]:
-        pattern = r"([NS])" if coord == "lat" else r"([EW])"
-        char_minus = "S" if coord == "lat" else "W"
-        dtype = pl.Int8 if coord == "lat" else pl.Int16
-        df = (
-            df.with_columns(  # 小文字を大文字に変更
-                pl.col(coord).str.to_uppercase(),
-            )
-            .with_columns(  # 左右を分離
-                pl.col(coord).str.split("~"),
-            )
-            .with_columns(  # 別の列へ分離
-                [
-                    pl.col(coord).list.get(num).alias(f"{coord}_{num}")
-                    for num in [0, 1]
-                ],
-            )
-            .drop(coord)
-            .with_columns(  # 右が存在しなければ左で埋める
-                pl.col(f"{coord}_1").fill_null(pl.col(f"{coord}_0")),
-            )
-            .with_columns(  # 東西南北で表された方位を分離
-                [
-                    pl.col(f"{coord}_{num}")
-                    .str.extract(pattern)
-                    .alias(f"{coord}_{num}_sign")
-                    for num in [0, 1]
-                ],
-            )
-            .with_columns(  # 東西南北の文字を消去し整数へ
-                [
-                    pl.col(f"{coord}_{num}")
-                    .str.replace(pattern, "")
-                    .str.replace_many(["P", "M"], ["+", "-"])  # P -> +, M -> -
-                    .cast(pl.Float64)
-                    .round(0)  #  四捨五入
-                    for num in [0, 1]
-                ],
-            )
-            .with_columns(  # 右の文字が存在しなければ左の文字で埋める
-                pl.col(f"{coord}_1_sign").fill_null(pl.col(f"{coord}_0_sign")),
-            )
-            .with_columns(  # 文字を符号に反映
-                [
-                    pl.when(pl.col(f"{coord}_{num}_sign").eq(char_minus))
-                    .then(-pl.col(f"{coord}_{num}"))
-                    .otherwise(pl.col(f"{coord}_{num}"))
-                    for num in [0, 1]
-                ],
-            )
-            .drop([f"{coord}_{num}_sign" for num in [0, 1]])
-            .with_columns(  # 左右の大小関係を修正
-                pl.min_horizontal(f"{coord}_0", f"{coord}_1")
-                .cast(dtype)
-                .alias(f"{coord}_min"),
-                pl.max_horizontal(f"{coord}_0", f"{coord}_1")
-                .cast(dtype)
-                .alias(f"{coord}_max"),
-            )
-            .drop([f"{coord}_{num}" for num in [0, 1]])
+def convert_coord(
+    df: pl.LazyFrame,
+    *,
+    col: str,
+    dtype: pl.PolarsDataType,
+) -> pl.LazyFrame:
+    pat_left_sign = r"(?P<left_sign>[nsewpm+-]?)"
+    pat_left = r"(?P<left>\d{1,2}(?:\.\d+)?)"
+    pat_right_sign = r"(?P<right_sign>[nsewpm+-]?)"
+    pat_right = r"(?P<right>\d{1,2}(?:\.\d+)?)"
+    pat = rf"(?i){pat_left_sign}{pat_left}(?:~{pat_right_sign}{pat_right})?"
+    return (
+        df.with_columns(
+            # 正規表現で構造体へ分解
+            pl.col(col).str.extract_groups(pat),
         )
-    return df
+        .unnest(col)  # 構造体から列へ分解
+        .with_columns(
+            # 符号の文字を小文字へ変換
+            pl.col("left_sign", "right_sign").str.to_lowercase(),
+        )
+        .with_columns(
+            # 右の符号と数値が存在しなければ左で埋める
+            pl.col("right_sign").fill_null(pl.col("left_sign")),
+            pl.col("right").fill_null(pl.col("left")),
+        )
+        .with_columns(
+            # 右の符号が存在せず、左の符号が東西南北のマイナスの場合
+            # 右の符号を左の符号で埋める
+            pl.when(
+                pl.col("right_sign").eq("")
+                & pl.col("left_sign").is_in({"s", "w"}),
+            )
+            .then(pl.lit("-"))
+            .otherwise(pl.col("right_sign"))
+            .alias("right_sign"),
+        )
+        .with_columns(
+            # 文字の符号を数式の符号へ変換
+            pl.col("left_sign", "right_sign").str.replace_many(
+                ["n", "s", "e", "w", "p", "m"],
+                ["+", "-", "+", "-", "+", "-"],
+            ),
+        )
+        .with_columns(
+            # 符号を数値へ反映
+            pl.concat_str("left_sign", "left").alias("left"),
+            pl.concat_str("right_sign", "right").alias("right"),
+        )
+        .with_columns(
+            # 文字列から小数へ変換し、四捨五入
+            pl.col("left", "right")
+            .cast(pl.Float64)
+            .round(),
+        )
+        .with_columns(
+            # 最大値と最小値を算出し、整数へ変換
+            pl.min_horizontal("left", "right").cast(dtype).alias(f"{col}_min"),
+            pl.max_horizontal("left", "right").cast(dtype).alias(f"{col}_max"),
+        )
+        .drop("left_sign", "right_sign", "left", "right")
+    )
 
 
 def split(df: pl.LazyFrame) -> tuple[pl.LazyFrame, pl.LazyFrame]:
@@ -163,7 +162,8 @@ def main() -> None:
     df_all = pl.concat(dfl).pipe(convert_number).pipe(convert_date)
 
     df_spot, df_nospot = split(df_all)
-    df_spot = df_spot.pipe(convert_lat)
+    df_spot = convert_coord(df_spot, col="lat", dtype=pl.Int8)
+    df_spot = convert_coord(df_spot, col="lon", dtype=pl.Int16)
 
     df_ar = df_spot.drop("num").pipe(sort_ar).collect()
     print("ar data:")

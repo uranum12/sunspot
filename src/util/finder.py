ac02046b789ee411f6b65ee11515bb660dc2e4a2
uranum12 <113694643+uranum12@users.noreply.github.com>
2024-01-02 14:37:40 +0000

    update util scripts

--- src/util/finder.py
+++ src/util/finder.py
@@ -4,29 +4,35 @@ from pathlib import Path
 
 import polars as pl
 
-if len(sys.argv) > 2:  # noqa: PLR2004
-    match sys.argv[1]:
-        case "ar":
-            ns = sys.argv[2][0:1].upper()
-            no = int(sys.argv[2][1:])
-            print(ns, no)
-            with pl.Config() as cfg:
-                cfg.set_tbl_rows(-1)
+
+def main(argv: list[str]) -> None:
+    if len(argv) > 2:  # noqa: PLR2004
+        match argv[1]:
+            case "ar":
+                ns = argv[2][0:1].upper()
+                no = int(argv[2][1:])
+                print(ns, no)
+                with pl.Config() as cfg:
+                    cfg.set_tbl_rows(-1)
+                    print(
+                        pl.scan_parquet(Path("out/ar/all.parquet"))
+                        .filter(pl.col("ns").eq(ns))
+                        .filter(pl.col("no").is_between(no - 1, no + 1))
+                        .select("ns", "no", "first", "last")
+                        .collect(),
+                    )
+            case "sn":
+                year, month, day = map(int, argv[2].split("-"))
+                target_date = date(year, month, day)
+                start_date = target_date - timedelta(days=1)
+                end_date = target_date + timedelta(days=1)
+                print(target_date)
                 print(
-                    pl.scan_parquet(Path("out/ar/all.parquet"))
-                    .filter(pl.col("ns").eq(ns))
-                    .filter(pl.col("no").is_between(no - 1, no + 1))
-                    .select("ns", "no", "first", "last")
+                    pl.scan_parquet(Path("out/sn/all.parquet"))
+                    .filter(pl.col("date").is_between(start_date, end_date))
                     .collect(),
                 )
-        case "sn":
-            year, month, day = map(int, sys.argv[2].split("-"))
-            target_date = date(year, month, day)
-            start_date = target_date - timedelta(days=1)
-            end_date = target_date + timedelta(days=1)
-            print(target_date)
-            print(
-                pl.scan_parquet(Path("out/sn/all.parquet"))
-                .filter(pl.col("date").is_between(start_date, end_date))
-                .collect(),
-            )
+
+
+if __name__ == "__main__":
+    main(sys.argv)
--- src/util/rename.py
+++ src/util/rename.py
@@ -1,25 +1,36 @@
 from pathlib import Path
 
-ar_path = Path("data/fujimori_ar")
-sn_path = Path("data/fujimori_sn")
 
-for data_path in [ar_path, sn_path]:
-    for file in data_path.glob("* - *.csv"):
-        month_dict = {
-            "jan": "1",
-            "feb": "2",
-            "mar": "3",
-            "apr": "4",
-            "may": "5",
-            "jun": "6",
-            "jul": "7",
-            "aug": "8",
-            "sep": "9",
-            "oct": "10",
-            "nov": "11",
-            "dec": "12",
-        }
-        year, _, month = file.stem.split()
-        month = month_dict.get(month, f"err-{month}")
-        new_name = f"{year}-{month_dict.get(month, month)}.csv"
+def main() -> None:
+    ar_path = Path("data/fujimori_ar")
+    sn_path = Path("data/fujimori_sn")
+
+    for data_path in [ar_path, sn_path]:
+        for file in data_path.glob("* - *.csv"):
+            month_dict = {
+                "jan": "1",
+                "feb": "2",
+                "mar": "3",
+                "apr": "4",
+                "may": "5",
+                "jun": "6",
+                "jul": "7",
+                "aug": "8",
+                "sep": "9",
+                "oct": "10",
+                "nov": "11",
+                "dec": "12",
+            }
+            year, _, month = file.stem.split()
+            month = month_dict.get(month, f"err-{month}")
+            new_name = f"{year}-{month_dict.get(month, month)}.csv"
+            file.rename(file.with_name(new_name))
+
+    for file in Path("data/seiryo").glob("* - *.csv"):
+        year, _, b0 = file.stem.split()
+        new_name = f"{year}-{b0}.csv"
         file.rename(file.with_name(new_name))
+
+
+if __name__ == "__main__":
+    main()

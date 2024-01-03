de4e38b43dbae9666384042aa9ca97238a7fe523
uranum12 <113694643+uranum12@users.noreply.github.com>
2024-01-03 15:03:49 +0000

    add seiryo observation days script

---
+++ src/seiryo_obs_days.py
@@ -0,0 +1,142 @@
+from datetime import date
+from pathlib import Path
+
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import polars as pl
+from dateutil.relativedelta import relativedelta
+from matplotlib.figure import Figure
+
+
+def calc_date_range(df: pl.LazyFrame) -> tuple[date, date]:
+    """日付の開始日と最終日を算出する
+
+    Args:
+        df (pl.LazyFrame): 黒点数データ
+
+    Returns:
+        tuple[date, date]: 開始日と最終日
+    """
+    date_range: dict[str, date] = (
+        df.select(
+            pl.min("date").alias("start"),
+            pl.max("date").alias("end"),
+        )
+        .collect()
+        .to_dicts()[0]
+    )
+    return date_range["start"], date_range["end"]
+
+
+def adjust_dates(start: date, end: date) -> tuple[date, date]:
+    """日付の範囲を月ごとに調整する
+
+    Args:
+        start (date): 開始日
+        end (date): 最終日
+
+    Returns:
+        tuple[date, date]: 開始日と最終日
+    """
+    start = start.replace(day=1)
+    end += relativedelta(months=1, day=1, days=-1)
+    return start, end
+
+
+def calc_dayly_obs(df: pl.LazyFrame, start: date, end: date) -> pl.LazyFrame:
+    """日ごとの観測日数を算出する
+
+    Args:
+        df (pl.LazyFrame): 黒点数データ
+        start (date): 開始日
+        end (date): 終了日
+
+    Returns:
+        pl.LazyFrame: 日ごとの観測
+    """
+
+    return (
+        pl.LazyFrame(
+            {"date": pl.date_range(start, end, interval="1d", eager=True)},
+        )
+        .join(
+            df.select("date").with_columns(pl.lit(1).alias("obs")),
+            on="date",
+            how="left",
+        )
+        .with_columns(pl.col("obs").fill_null(0).cast(pl.UInt8))
+    )
+
+
+def calc_monthly_obs(df: pl.LazyFrame) -> pl.LazyFrame:
+    """月ごとの観測日数を算出する
+
+    Args:
+        df (pl.LazyFrame): 日ごとの観測日数データ
+
+    Returns:
+        pl.LazyFrame: 月ごとの観測日数
+    """
+    return (
+        df.with_columns(pl.col("date").dt.truncate("1mo"))
+        .group_by("date")
+        .agg(pl.col("obs").sum().cast(pl.UInt8))
+        .sort("date")
+    )
+
+
+def draw_monthly_obs_days(df: pl.DataFrame) -> Figure:
+    """月ごとの観測日数のグラフを作成する
+
+    Args:
+        df (pl.DataFrame): 月ごとの観測日数
+
+    Returns:
+        Figure: 作成したグラフ
+    """
+    fig = plt.figure(figsize=(8, 5))
+    ax = fig.add_subplot(111)
+
+    ax.bar(df["date"], df["obs"], width=15)
+
+    ax.set_title("observations days per month")
+    ax.set_xlabel("date")
+    ax.set_ylabel("observations days")
+
+    ax.xaxis.set_major_locator(locator := mdates.AutoDateLocator())
+    ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(locator))
+
+    ax.set_xticks(ax.get_xticks())
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
+
+    fig.tight_layout()
+
+    return fig
+
+
+def main() -> None:
+    data_file = Path("out/seiryo/sn.parquet")
+    output_path = Path("out/seiryo")
+
+    df = pl.scan_parquet(data_file)
+
+    start, end = adjust_dates(*calc_date_range(df))
+
+    df_daily = calc_dayly_obs(df, start, end).collect()
+    print(df_daily)
+    df_daily.write_parquet(output_path / "observations_daily.parquet")
+
+    df_monthly = calc_monthly_obs(df_daily.lazy()).collect()
+    print(df_monthly)
+    df_monthly.write_parquet(output_path / "observations_monthly.parquet")
+
+    fig = draw_monthly_obs_days(df_monthly)
+    for ext in "pdf", "png":
+        file_path = output_path / f"observations_days.{ext}"
+        fig.savefig(file_path, dpi=300, bbox_inches="tight", pad_inches=0.1)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_seiryo_obs_days.py
@@ -0,0 +1,282 @@
+from datetime import date
+
+import polars as pl
+import pytest
+from polars.testing import assert_frame_equal
+
+import seiryo_obs_days
+
+
+@pytest.mark.parametrize(
+    ("in_date", "out_start", "out_end"),
+    [
+        (
+            [
+                date(2020, 2, 2),
+                date(2020, 1, 1),
+                date(2020, 3, 3),
+                date(2020, 5, 5),
+                date(2020, 4, 4),
+            ],
+            date(2020, 1, 1),
+            date(2020, 5, 5),
+        ),
+        (
+            [
+                date(2020, 3, 3),
+                date(2020, 3, 3),
+                date(2020, 5, 5),
+                date(2020, 3, 3),
+                date(2020, 12, 25),
+            ],
+            date(2020, 3, 3),
+            date(2020, 12, 25),
+        ),
+        (
+            [
+                date(2020, 2, 2),
+            ],
+            date(2020, 2, 2),
+            date(2020, 2, 2),
+        ),
+    ],
+)
+def test_calc_date_range(
+    in_date: list[date],
+    out_start: date,
+    out_end: date,
+) -> None:
+    df = pl.LazyFrame(
+        {
+            "date": in_date,
+        },
+        schema={
+            "date": pl.Date,
+        },
+    )
+    start, end = seiryo_obs_days.calc_date_range(df)
+    assert start == out_start
+    assert end == out_end
+
+
+@pytest.mark.parametrize(
+    ("in_start", "in_end", "out_start", "out_end"),
+    [
+        (
+            date(2020, 3, 3),
+            date(2020, 5, 5),
+            date(2020, 3, 1),
+            date(2020, 5, 31),
+        ),
+        (
+            date(2020, 1, 1),
+            date(2020, 2, 2),
+            date(2020, 1, 1),
+            date(2020, 2, 29),
+        ),
+        (
+            date(2020, 12, 25),
+            date(2021, 2, 2),
+            date(2020, 12, 1),
+            date(2021, 2, 28),
+        ),
+    ],
+)
+def test_adjust_dates(
+    in_start: date,
+    in_end: date,
+    out_start: date,
+    out_end: date,
+) -> None:
+    start, end = seiryo_obs_days.adjust_dates(in_start, in_end)
+    assert start == out_start
+    assert end == out_end
+
+
+@pytest.mark.parametrize(
+    ("in_date", "in_start", "in_end", "out_date", "out_obs"),
+    [
+        (
+            [
+                date(2020, 2, 2),
+                date(2020, 2, 5),
+                date(2020, 2, 6),
+                date(2020, 2, 8),
+                date(2020, 2, 4),
+            ],
+            date(2020, 2, 1),
+            date(2020, 2, 10),
+            [
+                date(2020, 2, 1),
+                date(2020, 2, 2),
+                date(2020, 2, 3),
+                date(2020, 2, 4),
+                date(2020, 2, 5),
+                date(2020, 2, 6),
+                date(2020, 2, 7),
+                date(2020, 2, 8),
+                date(2020, 2, 9),
+                date(2020, 2, 10),
+            ],
+            [
+                0,  # 2/1
+                1,  # 2/2
+                0,  # 2/3
+                1,  # 2/4
+                1,  # 2/5
+                1,  # 2/6
+                0,  # 2/7
+                1,  # 2/8
+                0,  # 2/9
+                0,  # 2/10
+            ],
+        ),
+        (
+            [
+                date(2020, 2, 2),
+                date(2020, 2, 5),
+                date(2020, 2, 6),
+                date(2020, 2, 8),
+                date(2020, 2, 4),
+            ],
+            date(2020, 2, 3),
+            date(2020, 2, 5),
+            [
+                date(2020, 2, 3),
+                date(2020, 2, 4),
+                date(2020, 2, 5),
+            ],
+            [
+                0,  # 2/3
+                1,  # 2/4
+                1,  # 2/5
+            ],
+        ),
+    ],
+)
+def test_calc_dayly_obs(
+    in_date: list[date],
+    in_start: date,
+    in_end: date,
+    out_date: list[date],
+    out_obs: list[int],
+) -> None:
+    df_in = pl.LazyFrame(
+        {
+            "date": in_date,
+        },
+        schema={
+            "date": pl.Date,
+        },
+    )
+    df_expected = pl.LazyFrame(
+        {
+            "date": out_date,
+            "obs": out_obs,
+        },
+        schema={
+            "date": pl.Date,
+            "obs": pl.UInt8,
+        },
+    )
+    df_out = seiryo_obs_days.calc_dayly_obs(df_in, in_start, in_end)
+    assert_frame_equal(df_out, df_expected)
+
+
+@pytest.mark.parametrize(
+    ("in_date", "in_obs", "out_date", "out_obs"),
+    [
+        (
+            [
+                date(2020, 5, 30),
+                date(2020, 5, 31),
+                date(2020, 6, 1),
+                date(2020, 6, 2),
+                date(2020, 6, 3),
+            ],
+            [
+                1,  # 5/30
+                1,  # 5/31
+                0,  # 6/1
+                1,  # 6/2
+                0,  # 6/3
+            ],
+            [
+                date(2020, 5, 1),
+                date(2020, 6, 1),
+            ],
+            [
+                2,
+                1,
+            ],
+        ),
+        (
+            [
+                date(2020, 5, 30),
+                date(2020, 5, 31),
+                date(2020, 6, 1),
+                date(2020, 8, 7),
+                date(2020, 8, 8),
+            ],
+            [
+                1,  # 5/30
+                1,  # 5/31
+                0,  # 6/1
+                1,  # 8/7
+                0,  # 8/8
+            ],
+            [
+                date(2020, 5, 1),
+                date(2020, 6, 1),
+                date(2020, 8, 1),
+            ],
+            [
+                2,
+                0,
+                1,
+            ],
+        ),
+    ],
+)
+def test_calc_monthly_obs(
+    in_date: list[date],
+    in_obs: list[int],
+    out_date: list[date],
+    out_obs: list[int],
+) -> None:
+    df_in = pl.LazyFrame(
+        {
+            "date": in_date,
+            "obs": in_obs,
+        },
+        schema={
+            "date": pl.Date,
+            "obs": pl.UInt8,
+        },
+    )
+    df_expected = pl.LazyFrame(
+        {
+            "date": out_date,
+            "obs": out_obs,
+        },
+        schema={
+            "date": pl.Date,
+            "obs": pl.UInt8,
+        },
+    )
+    df_out = seiryo_obs_days.calc_monthly_obs(df_in)
+    assert_frame_equal(df_out, df_expected)
+
+
+def test_draw_monthly_obs_days() -> None:
+    df = pl.DataFrame(
+        {
+            "date": [date(2020, 1, 1), date(2020, 2, 1), date(2020, 3, 1)],
+            "obs": [3, 0, 2],
+        },
+        schema={
+            "date": pl.Date,
+            "obs": pl.UInt8,
+        },
+    )
+    _ = seiryo_obs_days.draw_monthly_obs_days(df)

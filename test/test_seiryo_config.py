6ffd9f613692ea20a1e7e0bd80d436a48857c240
uranum12 <113694643+uranum12@users.noreply.github.com>
2024-06-23 14:25:23 +0900

    add default config test

--- test/test_seiryo_config.py
+++ test/test_seiryo_config.py
@@ -4,10 +4,23 @@ from pathlib import Path
 import pytest
 from pydantic import BaseModel
 
+from seiryo_butterfly_config import ButterflyDiagram
 from seiryo_obs_days_config import ObservationsMonthly
-from seiryo_sunspot_number import (
+from seiryo_sunspot_number_config import (
     SunspotNumberHemispheric,
     SunspotNumberWholeDisk,
+)
+from seiryo_sunspot_number_with_flare_config import (
+    SunspotNumberWithFlare,
+    SunspotNumberWithFlareHemispheric,
+)
+from seiryo_sunspot_number_with_silso_config import (
+    SunspotNumberDiff,
+    SunspotNumberRatio,
+    SunspotNumberRatioDiff1,
+    SunspotNumberRatioDiff2,
+    SunspotNumberScatter,
+    SunspotNumberWithSilso,
 )
 
 
@@ -23,7 +36,45 @@ from seiryo_sunspot_number import (
             SunspotNumberHemispheric,
         ),
         pytest.param(
+            "config/seiryo/sunspot_number/with_flare.json",
+            SunspotNumberWithFlare,
+        ),
+        pytest.param(
+            "config/seiryo/sunspot_number/with_flare_hemispheric.json",
+            SunspotNumberWithFlareHemispheric,
+        ),
+        pytest.param(
+            "config/seiryo/sunspot_number/with_silso.json",
+            SunspotNumberWithSilso,
+        ),
+        pytest.param(
+            "config/seiryo/sunspot_number/scatter.json", SunspotNumberScatter
+        ),
+        pytest.param(
+            "config/seiryo/sunspot_number/ratio.json", SunspotNumberRatio
+        ),
+        pytest.param(
+            "config/seiryo/sunspot_number/diff.json", SunspotNumberDiff
+        ),
+        pytest.param(
+            "config/seiryo/sunspot_number/ratio_diff_1.json",
+            SunspotNumberRatioDiff1,
+        ),
+        pytest.param(
+            "config/seiryo/sunspot_number/ratio_diff_2.json",
+            SunspotNumberRatioDiff2,
+        ),
+        pytest.param(
             "config/seiryo/observations/monthly.json", ObservationsMonthly
+        ),
+        pytest.param(
+            "config/seiryo/butterfly_diagram/monthly.json", ButterflyDiagram
+        ),
+        pytest.param(
+            "config/seiryo/butterfly_diagram/fromtext.json", ButterflyDiagram
+        ),
+        pytest.param(
+            "config/seiryo/butterfly_diagram/merged.json", ButterflyDiagram
         ),
     ],
 )
